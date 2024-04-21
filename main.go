128a104666e3e18dc26d1e874cba11df9d6f54d9
Lucas-Beijato <lucasbeijato0@gmail.com>
2024-04-20 23:26:58 -0300

    Testando o import de funcs para Handlers.

--- main.go
+++ main.go
@@ -18,11 +18,14 @@ func main() {
 	// v1 version
 	v1 := app.Group("/v1")
 
+	Handler_newSale := webhooks.New_Sale_Handle
+	Handler_Val_tok := routes.Validate_Token_Handle
+
 	// WebHooks
-	v1.Post("/newSale", webhooks.New_Sale_Handle)
+	v1.Post("/newSale", Handler_newSale)
 
 	// Routes
-	v1.Post("/validate_token", routes.Validate_Token_Handle)
+	v1.Post("/validate_token", Handler_Val_tok)
 
 	// Start Server
 	fmt.Println("[App]: Essa bagaça ta rodando! 🚀 ")
