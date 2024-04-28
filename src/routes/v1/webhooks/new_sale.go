9431841444e098feae8e24048afe2e63ea15efd6
Lucas-Beijato <lucasbeijato0@gmail.com>
2024-04-28 15:15:33 -0300

    capturando cabeçalhos 4

--- src/routes/v1/webhooks/new_sale.go
+++ src/routes/v1/webhooks/new_sale.go
@@ -15,11 +15,8 @@ func New_Sale_Handler(c *fiber.Ctx) error {
 
 	fmt.Println("[app]: Entrada no webhook '/new_sale'")
 
-	headers := c.GetReqHeaders()
-	fmt.Println(headers)
-
-	headers = c.GetRespHeaders()
-	fmt.Println(headers)
+	cookies := c.Cookies("Token")
+	fmt.Println(cookies)
 
 	b := new(req_res_types.KiwifyResponse)
 	if err := c.BodyParser(b); err != nil {
