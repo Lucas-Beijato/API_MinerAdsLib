97ea4ac146a727b0e33191f1f54b56129041d6c5
Lucas-Beijato <lucasbeijato0@gmail.com>
2024-04-30 10:29:03 -0300

    Testando todos os métodos de hash

--- src/services/validate_signature/main.go
+++ src/services/validate_signature/main.go
@@ -3,18 +3,42 @@ package validatesignature
 import (
 	"crypto/hmac"
 	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
 	"fmt"
 )
 
 // Func - Validate Signature
 func ValidateSignature(req_message []byte, req_message_signature []byte, token []byte) bool {
+
+	// Teste de métodos
+	metodo224 := hmac.New(sha256.New224, token)
+	metodo224.Write(req_message)
+	expectedMAC224 := metodo224.Sum(nil)
+	fmt.Println("metodo224: ", expectedMAC224)
+
+	metodo256 := hmac.New(sha256.New, token)
+	metodo256.Write(req_message)
+	expectedMAC256 := metodo256.Sum(nil)
+	fmt.Println("metodo256: ", expectedMAC256)
+
+	metodo384 := hmac.New(sha512.New384, token)
+	metodo384.Write(req_message)
+	expectedMAC384 := metodo384.Sum(nil)
+	fmt.Println("metodo384: ", expectedMAC384)
+
+	metodo512 := hmac.New(sha512.New, token)
+	metodo512.Write(req_message)
+	expectedMAC512 := metodo512.Sum(nil)
+	fmt.Println("metodo512: ", expectedMAC512)
+
+	// Método principal
 	true_signature := hmac.New(sha1.New, token)
 	true_signature.Write(req_message)
 	expectedMAC := true_signature.Sum(nil)
 
 	fmt.Println("Mensagem recebida", string(req_message))
 	fmt.Println("Assinatura recebida", string(req_message_signature))
-	fmt.Println("Token do sistema \n", string(token))
 	fmt.Println("Comparando as chaves:")
 	fmt.Println(req_message_signature)
 	fmt.Println(expectedMAC)
